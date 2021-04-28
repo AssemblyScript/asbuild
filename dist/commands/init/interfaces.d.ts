9e3c8c4de2cb464cc597f63035d6f3e120c5c6ad
Ashutosh Varma <ashutoshvarma11@live.com>
2021-04-29 00:37:22 +0530

    update dist/

--- dist/commands/init/interfaces.d.ts
+++ dist/commands/init/interfaces.d.ts
@@ -1,19 +1,17 @@
 export declare enum InitResult {
     UPDATED = 0,
     CREATED = 1,
-    EXISTS = 2,
-    OVERWRITE = 3
+    EXISTS = 2
 }
 export declare abstract class InitFile {
     constructor();
     abstract path: string;
     abstract updateOldContent: ((old: string) => string) | null;
     abstract getContent(): string;
+    getRelativePath(baseDir: string): string;
     /**
      * Write the InitFile to given baseDir
      * @param baseDir Base directory where file will created in relative to
-     * @param overwrite Whether to overwrite file if current InitFile does not
-     *                  support updating old file content.
      */
-    write(baseDir: string, overwrite?: boolean): InitResult;
+    write(baseDir: string): InitResult;
 }
