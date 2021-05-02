ef2edfe9cf48c486f25b64f2730ed04cd510ea3d
Ashutosh Varma <ashutoshvarma11@live.com>
2021-05-02 21:24:59 +0530

    add uliloty functions for testing

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,7 @@ import * as asc from "assemblyscript/cli/asc";
 import * as path from "path";
 import * as fs from "fs";
 import * as readline from "readline";
+import * as sinon from "sinon";
 
 const DEFAULT_ASC_OPTIONS: asc.APIOptions = {};
 const DEFAULT_CLI_CALLBACK: (a: any) => number = (err) => {
@@ -60,6 +61,29 @@ export async function askYesNo(ques: string): Promise<boolean> {
   const res: string = await new Promise((resolve) =>
     rl.question(ques + " [Y/n]: ", resolve)
   );
-  rl.close()
+  rl.close();
   return /^$|^y?$/.test(res.toLowerCase().trim());
+}
+
+export function log(message?: any, error = false) {
+  error ? console.error(message) : console.info(message);
+}
+
+export function mockModule<T extends { [K: string]: any }>(
+  moduleToMock: T,
+  defaultMockValuesForMock: Partial<{ [K in keyof T]: T[K] }>
+) {
+  return (
+    sandbox: sinon.SinonSandbox,
+    returnOverrides?: Partial<{ [K in keyof T]: T[K] }>
+  ): void => {
+    const returns: any = returnOverrides || {};
+    const functions = Object.keys(moduleToMock);
+    functions.forEach((f) => {
+      const override = returns[f] || defaultMockValuesForMock[f];
+      if (override) {
+        sandbox.stub(moduleToMock, f).callsFake(override);
+      }
+    });
+  };
 }
